3e36a5ae420d6c241564e8f686d9c9e706795062
chriswill88 <williechri79@gmail.com>
2020-04-16 23:27:32 +0000

    [FIX] added helper function, added a fail case

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -6,17 +6,30 @@
 """
 
 
+def dimensions_getter(var):
+    i = 0
+    while isinstance(var, list):
+        # print("from dim_g var is list and ", var)
+        var = var[0]
+        i += 1
+    return i
+
+
 def cat_matrices2D(mat1, mat2, axis=0):
     """
         this function concatinates matrixes on a given axis
     """
+
+    if dimensions_getter(mat1) != dimensions_getter(mat2):
+        return None
+    if (dimensions_getter(mat1) != 2):
+        return None
+
     newy = []
-    z = 0
-    for i in mat1:
+    for i in range(len(mat1)):
         newy.append([])
-        for x in i:
-            newy[z].append(x)
-        z += 1
+        for x in mat1[i]:
+            newy[i].append(x)
 
     if (axis == 0):
         for i in mat2:
