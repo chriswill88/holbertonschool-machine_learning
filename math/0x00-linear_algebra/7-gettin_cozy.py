260fee03f0822e2afafe1ece22735af89167d6d9
chriswill88 <williechri79@gmail.com>
2020-04-16 22:43:18 +0000

    [FIX] use deep copy

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
+import copy
+"""
+    In this modual
+    there is the function
+    cat_matrixs2D for task 7
+"""
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
-    newy = []
-    newy = mat1.copy()
-
-    # print("1:{} 2:{}".format(mat1, mat2))
-    # print("newy = ", newy)
-
+    """
+        this function concatinates matrixes on a given axis
+    """
+    newy = copy.deepcopy(mat1)
     if (axis == 0):
         for i in mat2:
             newy.append(i)
-    if (axis == 1):
+    elif (axis == 1):
         for i in range(len(mat2)):
             newy[i].extend(mat2[i])
-
-    # print("--final", newy, "\n", mat1, "\n", mat2)
     return newy
