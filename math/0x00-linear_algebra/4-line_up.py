bfbc17fdff6bfd4e4ea6fe2cbb79465643a470b7
chriswill88 <williechri79@gmail.com>
2020-04-16 07:33:16 +0000

    [ADD] task 4

---
+++ math/0x00-linear_algebra/4-line_up.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+    In this modual there is the
+    add_array function
+    for task 4.
+"""
+
+
+def add_arrays(arr1, arr2):
+    """
+        add_array - does element wise addition
+    """
+    if (len(arr1) != len(arr2)):
+        return (None)
+    lis = []
+    for i in range(len(arr1)):
+        lis.append(arr1[i] + arr2[i])
+    return lis
