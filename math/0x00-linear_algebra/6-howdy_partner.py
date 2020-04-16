5ffbca8355b5bde362d57c8ef8294061cc448912
chriswill88 <williechri79@gmail.com>
2020-04-16 08:29:08 +0000

    [ADD] task 6

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+    In this modual
+    there is the function
+    cat_arrays for task 6
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+        cat_arrays - concatinates two arrays
+    """
+    return arr1 + arr2
