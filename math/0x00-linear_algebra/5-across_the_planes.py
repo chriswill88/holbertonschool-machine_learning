ed5d3ee9e4914c38729e6be2b0c95cb66e4e124a
chriswill88 <williechri79@gmail.com>
2020-04-16 08:21:49 +0000

    [FIX] remove import shape function

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -4,14 +4,13 @@
     there is the function
     add_matrices2d for task 5
 """
-shape = __import__('2-size_me_please').matrix_shape
 
 
 def add_matrices2D(mat1, mat2):
     """
         add_matrices2D - does element wise addition for matrices
     """
-    if (shape(mat1) != shape(mat2)):
+    if (len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0])):
         return None
 
     new = []
@@ -19,4 +18,5 @@ def add_matrices2D(mat1, mat2):
         new.append([])
         for x in range(len(mat1[0])):
             new[i].append(mat1[i][x] + mat2[i][x])
+
     return new
