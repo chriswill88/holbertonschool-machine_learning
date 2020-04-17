36e471ee25725f91db23ef8729675f4eeababf62
chriswill88 <williechri79@gmail.com>
2020-04-17 03:56:56 +0000

    [ADD] task 13

---
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import numpy as np
+"""this modual hold the functions for task 10"""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """this function returns a transposed matrix"""
+    return np.concatenate(mat1, mat2, axis)
