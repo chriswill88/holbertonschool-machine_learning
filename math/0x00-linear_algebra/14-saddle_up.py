ca68e38ee230618f6cbc170bd78197c2cabb3073
chriswill88 <williechri79@gmail.com>
2020-04-17 04:22:49 +0000

    [ADD] task 14

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""this modual hold the functions for task 14"""
+
+
+def np_matmul(mat1, mat2):
+    """this function returns a product of matrix multiplication"""
+    return mat1.dot(mat2)
