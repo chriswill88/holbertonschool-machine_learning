b3f02a2a009e9ab7afc7f0750b925f0a47f2c372
chriswill88 <williechri79@gmail.com>
2020-04-17 04:17:49 +0000

    [ADD] task 12

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""this modual hold the functions for task 10"""
+
+
+def np_elementwise(mat1, mat2):
+    """this function element wise matrix math"""
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1/mat2)
