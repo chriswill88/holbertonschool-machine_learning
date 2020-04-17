7bb57edf01b97438ec27c498c1cfde7b36264efc
chriswill88 <williechri79@gmail.com>
2020-04-17 06:36:46 +0000

    [FIX-TEST]

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -7,12 +7,15 @@
 
 
 def dimensions_getter(var):
-    """returns the number of dimensions"""
+    """returns the shape"""
     i = 0
     lis = []
     while isinstance(var, list):
         lis.append(len(var))
-        var = var[0]
+        try:
+            var = var[0]
+        except IndexError:
+            return lis
         i += 1
     return list(reversed(lis))
 
@@ -20,27 +23,28 @@ def dimensions_getter(var):
 def cat_matrices2D(mat1, mat2, axis=0):
     """this function concatinates matrixes on a given axis"""
 
-    shape1 = dimensions_getter(mat1)
-    shape2 = dimensions_getter(mat2)
+    # shape1 = dimensions_getter(mat1)
+    # shape2 = dimensions_getter(mat2)
+    # print(shape1, shape)
 
-    if (len(shape1) != 2 or len(shape2) != 2):
-        return (None)
-    try:
-        if (shape1[axis] != shape2[axis]):
-            return None
-    except Exception:
-        return None
+    # if (len(shape1) != 2 or len(shape2) != 2):
+    #     return (None)
+
+    # if (shape1[axis] != shape2[axis]):
+    #     return None
 
     new = []
     for i in range(len(mat1)):
         new.append([])
         for x in mat1[i]:
             new[i].append(x)
-
-    if (axis == 0):
-        for i in mat2:
-            new.append(i)
-    elif (axis == 1):
-        for i in range(len(mat2)):
-            new[i].extend(mat2[i])
+    try:
+        if (axis == 0):
+            for i in mat2:
+                new.append(i)
+        elif (axis == 1):
+            for i in range(len(mat2)):
+                new[i].extend(mat2[i])
+    except IndexError:
+        return None
     return new
