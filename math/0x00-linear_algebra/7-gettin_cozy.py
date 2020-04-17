af35b8dc1186484d8d1a370ccd2449470d4aeec0
chriswill88 <williechri79@gmail.com>
2020-04-17 01:16:56 +0000

    [FIX] check dimension sizes added

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -23,22 +23,24 @@ def cat_matrices2D(mat1, mat2, axis=0):
     shape1 = dimensions_getter(mat1)
     shape2 = dimensions_getter(mat2)
 
+    if (len(shape1) != 2 or len(shape2) != 2):
+        return (None)
     try:
         if (shape1[axis] != shape2[axis]):
             return None
     except IndexError:
         return None
 
-    newy = []
+    new = []
     for i in range(len(mat1)):
-        newy.append([])
+        new.append([])
         for x in mat1[i]:
-            newy[i].append(x)
+            new[i].append(x)
 
     if (axis == 0):
         for i in mat2:
-            newy.append(i)
+            new.append(i)
     elif (axis == 1):
         for i in range(len(mat2)):
-            newy[i].extend(mat2[i])
-    return newy
+            new[i].extend(mat2[i])
+    return new
