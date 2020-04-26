9c55111faa93448dfad78125eae0241b5fa34ffc
chriswill88 <williechri79@gmail.com>
2020-04-26 11:18:34 +0000

    [FIX] task 9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,10 +6,6 @@ def summation_i_squared(n):
     """this function does sqared factorial from n to 0"""
     if not isinstance(n, int) or n < 0:
         return None
-
-    try:
-        if (n < 1):
-            return 0
-        return((n * n) + summation_i_squared(n-1))
-    except TypeError:
-        return None
+    if (n < 1):
+        return 0
+    return((n * n) + summation_i_squared(n-1))
