c72fd0c708f0945ff9995eb495d7466f94bcd641
chriswill88 <williechri79@gmail.com>
2020-04-26 11:29:06 +0000

    [Test]

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
+import sys
 """this modual holds the function for task 9"""
+sys.setrecursionlimit(1501)
 
 
 def summation_i_squared(n):
     """this function does sqared factorial from n to 0"""
     if not isinstance(n, int) or n <= 0:
         return None
-
     if (n < 2):
         return 1
     return((n * n) + summation_i_squared(n-1))
