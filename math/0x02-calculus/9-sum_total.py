b50cc4d201936439edbdc4d04a87cf6bf299984d
chriswill88 <williechri79@gmail.com>
2020-04-26 11:10:46 +0000

    [ADD] task 0-9

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""this modual holds the function for task 9"""
+
+
+def summation_i_squared(n):
+    """this function does sqared factorial from n to 0"""
+    if not isinstance(n, int) or n < 0:
+        return None
+
+    try:
+        if (n < 1):
+            return 0
+        return((n * n) + summation_i_squared(n-1))
+    except TypeError:
+        return None
