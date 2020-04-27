7110db8437149d7309102dc7977ba14975b8deee
chriswill88 <williechri79@gmail.com>
2020-04-27 03:36:30 +0000

    [REVERT CODE] task 9

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,8 +4,8 @@
 
 def summation_i_squared(n):
     """this function does sqared factorial from n to 0"""
-    if n < 0:
+    if n <= 0:
         return None
-    if (n < 1):
-        return 0
+    if (n < 2):
+        return 1
     return((n * n) + summation_i_squared(n - 1))
