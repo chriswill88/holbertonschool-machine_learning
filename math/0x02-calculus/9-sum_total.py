bb58b167d785b0f54963552a41a64220e3afcd0a
chriswill88 <williechri79@gmail.com>
2020-04-27 04:15:17 +0000

    [FIX] remove recursion

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -6,6 +6,6 @@ def summation_i_squared(n):
     """this function does sqared factorial from n to 0"""
     if n <= 0 or not isinstance(n, int):
         return None
-    if (n < 2):
-        return 1
-    return((n * n) + summation_i_squared(n - 1))
+    summ = list(map((lambda x: x * x), range(n + 1)))
+    return sum(summ)
+ 
