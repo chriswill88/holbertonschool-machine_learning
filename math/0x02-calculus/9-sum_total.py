8d9a4151be883e2b09c24778b380af7ce7745727
chriswill88 <williechri79@gmail.com>
2020-04-27 03:38:52 +0000

    [TEST]

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,7 +4,7 @@
 
 def summation_i_squared(n):
     """this function does sqared factorial from n to 0"""
-    if n <= 0:
+    if n <= 0 or not isinstance(n, int):
         return None
     if (n < 2):
         return 1
