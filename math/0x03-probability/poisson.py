00f5b64299ca053fcf45be31269f2b2f17d1ce01
chriswill88 <williechri79@gmail.com>
2020-04-30 15:11:17 +0000

    [FIX] lambtha must be positive

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -5,17 +5,14 @@
 class Poisson:
     """This class Represents the poisson distribution"""
     def __init__(self, data=None, lambtha=1.):
+        if lambtha < 0:
+            raise ValueError("lambtha must be a positive value")
         self.lambtha = float(lambtha)
         if data is None:
-            if lambtha < 0:
-                raise ValueError("lambtha must be a positive value")
             self.data = lambtha
         else:
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            total = 0
-            for i in data:
-                total += i
-            self.lambtha = total/len(data)
+            self.lambtha = sum(data)/len(data)
