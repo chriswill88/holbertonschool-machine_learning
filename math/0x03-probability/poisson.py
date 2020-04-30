7fb7b74c83c18f037fb74b6a1ed7d471d3a6f3e2
chriswill88 <williechri79@gmail.com>
2020-04-30 14:26:09 +0000

    [CLEAN] cleaned code in task 0

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -3,19 +3,14 @@
 
 
 class Poisson:
-    """
-    This class Represents the poisson distribution.
-    """
+    """This class Represents the poisson distribution"""
     def __init__(self, data=None, lambtha=1.):
         self.lambtha = float(lambtha)
-
         if data is None:
             if lambtha < 0:
                 raise ValueError("lambtha must be a positive value")
-            else:
-                self.data = lambtha
+            self.data = lambtha
         else:
-            # calculate the lambtha of data
             if not isinstance(data, list):
                 raise TypeError("data must be a list")
             if len(data) < 2:
