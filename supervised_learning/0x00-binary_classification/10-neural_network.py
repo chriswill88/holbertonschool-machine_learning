2558776c2f73accb3e9d34f95c718d08c71dd73c
chriswill88 <williechri79@gmail.com>
2020-05-13 19:56:22 +0000

    [ADD] task 10

---
+++ supervised_learning/0x00-binary_classification/10-neural_network.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""this modual contains the class NeuralNetwork"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    neuralnetwork - this class defines a neural network with one hidden layer
+    """
+    def __init__(self, nx, nodes):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
+
+    def forward_prop(self, X):
+        """
+        forward_prop figures out the
+        forward propogation for the
+        neural network with one hidden layer
+        """
+        W1 = self.__W1
+        W2 = self.__W2
+        b1 = self.__b1
+        b2 = self.__b2
+
+        Z1 = W1@X+b1
+        A1 = 1/(1+np.exp(-1 * Z1))
+        Z2 = W2@A1+b2
+        A2 = 1/(1+np.exp(-1 * Z2))
+
+        self.__A1, self.__A2 = A1, A2
+        return self.__A1, self.__A2
