b81c8099717424fdd84fbc5ce7c1371b7b248d1d
chriswill88 <williechri79@gmail.com>
2020-05-13 15:13:39 +0000

    [FIX] output

--- supervised_learning/0x00-binary_classification/8-neural_network.py
+++ supervised_learning/0x00-binary_classification/8-neural_network.py
@@ -10,15 +10,15 @@ class NeuralNetwork:
     def __init__(self, nx, nodes):
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
-        if nx <= 1:
+        if nx < 1:
             raise ValueError("nx must be a positive integer")
         if not isinstance(nodes, int):
             raise TypeError("nodes must be an integer")
-        if nodes <= 1:
+        if nodes < 1:
             raise ValueError("nodes must be a positive integer")
 
         self.W1 = np.random.randn(nodes, nx)
-        self.b1 = 0
+        self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
         self.W2 = np.random.randn(1, nodes)
         self.b2 = 0
