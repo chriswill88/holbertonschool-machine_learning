c4909d15b93d31113198fc2521fe763afc65ab62
chriswill88 <williechri79@gmail.com>
2020-05-15 16:35:56 +0000

    [FIX] task wrong type of error

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -13,7 +13,7 @@ class DeepNeuralNetwork:
         if not isinstance(layers, list):
             raise TypeError("layers must be a list of positive integers")
         if len(layers) == 0 or min(layers) < 1:
-            raise ValueError("layers must be a positive integer")
+            raise TypeError("layers must be a positive integer")
         n = np.random.rand
         self.L = len(layers)  # the number of layers
         self.cache = {}  # to hold all intermediary values of the network.
