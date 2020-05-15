278c4f3296077fabf293275728d6d9690ff4b325
chriswill88 <williechri79@gmail.com>
2020-05-15 17:37:02 +0000

    [FIX] wrong function randn instead of rand

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -10,7 +10,7 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list):
+        if not isinstance(layers, list) or layers is None:
             raise TypeError("layers must be a list of positive integers")
         if len(layers) == 0 or min(layers) < 1:
             raise TypeError("layers must be a positive integer")
