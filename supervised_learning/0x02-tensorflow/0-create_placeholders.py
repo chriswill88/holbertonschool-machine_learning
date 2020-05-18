5c4e9eedaf85187fb0cb277c5307624e0468950d
chriswill88 <williechri79@gmail.com>
2020-05-18 06:29:53 +0000

    [ADD] task README and task 0

---
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+import numpy as np
+
+
+def create_placeholders(nx, classes):
+    """a function that returns two placeholders"""
+    x = tf.placeholder(tf.float32, shape=[None, nx], name='x')
+    y = tf.placeholder(tf.float32, shape=[None, classes], name='y')
+    return x, y
