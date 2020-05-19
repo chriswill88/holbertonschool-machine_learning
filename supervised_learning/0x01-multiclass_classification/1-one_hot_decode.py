43ac556d0dfce8e8d0d3225029c504d5fe65c573
chriswill88 <williechri79@gmail.com>
2020-05-19 23:48:08 +0000

    [FIX] task 1

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -5,6 +5,9 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """that converts a one-hot matrix into a vector of labels"""
+    if not isinstance(one_hot, np.ndarray) or one_hot.ndim != 2:
+        return None
+
     classes, m = one_hot.shape
     onehot = np.zeros(m, int)
 
