30d69d371f3dd49c0b1ede5dfcabac3d1d7b6367
chriswill88 <williechri79@gmail.com>
2020-05-19 23:45:23 +0000

    [ADD] task 1

---
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""this modual holds function for task 1"""
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """that converts a one-hot matrix into a vector of labels"""
+    classes, m = one_hot.shape
+    onehot = np.zeros(m, int)
+
+    for i in range(m):
+        for x in range(classes):
+            if one_hot[x][i] == 1:
+                onehot[i] = x
+    return onehot
