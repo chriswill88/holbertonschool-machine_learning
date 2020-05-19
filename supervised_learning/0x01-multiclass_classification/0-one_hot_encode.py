531784c1acd502027105a310f7dec85621f4c343
chriswill88 <williechri79@gmail.com>
2020-05-19 21:52:05 +0000

    [FIX] the the null cases are added

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -6,6 +6,10 @@ import numpy as np
 def one_hot_encode(Y, classes):
     """converts a numeric label vector into a one-hot matrix"""
     m = len(Y)
+    if not isinstance(classes, int) or classes < 2:
+        return None
+    if classes < np.amax(Y):
+        return None
     onehot = np.zeros((classes, m))
     for i in range(m):
         onehot[Y[i]][i] = 1
