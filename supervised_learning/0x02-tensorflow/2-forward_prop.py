034134c5e49c9a04023ea06542004206d1581091
chriswill88 <williechri79@gmail.com>
2020-05-21 21:44:44 +0000

    [ADD]

---
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+create_layer = __import__('1-create_layer').create_layer
+"""this modual contains the function for task 2"""
+
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    """
+    x - placeholder for input data
+    layer_sizes - contain the number of nodes in each layer of the network
+    activations - the activation functions for each layer of the network
+    Return: is a list containing the prediction of network in tensor form
+    """
+
+    for i in range(len(layer_sizes)):
+        x = create_layer(x, layer_sizes[i], activations[i])
+    return x
