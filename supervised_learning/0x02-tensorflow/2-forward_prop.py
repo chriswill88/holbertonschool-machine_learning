acde2f514d0654f14278c729ffb2633ba77eec5b
chriswill88 <williechri79@gmail.com>
2020-05-21 21:46:02 +0000

    [ADD]

--- supervised_learning/0x02-tensorflow/2-forward_prop.py
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-create_layer = __import__('1-create_layer').create_layer
 """this modual contains the function for task 2"""
+create_layer = __import__('1-create_layer').create_layer
 
 
 def forward_prop(x, layer_sizes=[], activations=[]):
