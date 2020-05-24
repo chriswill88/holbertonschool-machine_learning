46224cd5c773e3333e5f3fdff6c1d2312b55bad2
chriswill88 <williechri79@gmail.com>
2020-05-24 11:46:21 +0000

    [ADD] task 7

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -7,16 +7,16 @@ def evaluate(X, Y, save_path):
     """ evaluates the output of a nerual network"""
     saver = tf.train.Saver
     sp = save_path
-    with tf.Session() as sess:
-        sess = saver.restore(sess, save_path=sp)
-        print("sess -> ", sess)
+    with tf.Session() as ses:
+        ses = saver.restore(sess=ses, save_path=sp)
+        print("ses -> ", ses)
         accuracy = tf.get_collection("accuracy")[0]
         loss = tf.get_collection("loss")[0]
         pred = tf.get_collection("y_pred")[0]
 
-        pre = sess.run(pred, feed_dict={"x": X, "y": Y})
-        acc = sess.run(accuracy, feed_dict={"x": X, "y": Y})
-        los = sess.run(loss, feed_dict={"x": X, "y": Y})
+        pre = ses.run(pred, feed_dict={"x": X, "y": Y})
+        acc = ses.run(accuracy, feed_dict={"x": X, "y": Y})
+        los = ses.run(loss, feed_dict={"x": X, "y": Y})
 
         print("prediction -> ", pred)
         print("acc -> ", acc)
