4af6558d46271d37bfd8bd4b341e7ad2967b7aea
chriswill88 <williechri79@gmail.com>
2020-05-24 09:11:53 +0000

    [ADD] task 6

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -19,8 +19,8 @@ def train(
     Y_train is a numpy.ndarray containing the training labels
     X_valid is a numpy.ndarray containing the validation input data
     Y_valid is a numpy.ndarray containing the validation labels
-    layer_sizes is a list containing the number of nodes in each layer of the network
-    activations is a list containing the activation functions for each layer of the network
+    layer_sizes contains the number of nodes in each layer of the network
+    activations contains the activation functions for each layer of the network
     alpha is the learning rate
     iterations is the number of iterations to train over
     save_path designates where to save the model
@@ -30,28 +30,43 @@ def train(
     vnx, vclass = X_valid.shape
 
     inputdata, one_hot = create_placeholders(nx, classes)
-    # print(inputdata, one_hot)
     predict = forward_prop(inputdata, layer_sizes, activations)
-    # print(predict)
     cost = calculate_loss(one_hot, predict)
-    # print(cost)
 
     accuracy = calculate_accuracy(one_hot, predict)
     train = create_train_op(cost, alpha)
-
-    # print("input -> ", inputdata, "\n predict -> ", predict, "\n acc -> ", accuracy, "\n cost -> ", cost, "\n Train -> ", train)
     ini = tf.global_variables_initializer()
 
     saver = tf.train.Saver
     sess = tf.Session()
     sess.run(ini)
 
+    tf.add_to_collection(name="x", value=inputdata)
+    tf.add_to_collection(name="y", value=one_hot)
+    tf.add_to_collection(name="y_pred", value=predict)
+    tf.add_to_collection(name="loss", value=cost)
+    tf.add_to_collection(name="accuracy", value=accuracy)
+    tf.add_to_collection(name="train_op", value=train)
+
     for i in range(iterations):
         sess.run(predict, feed_dict={inputdata: X_train, one_hot: Y_train})
-        # if i % iterations == 0 or i == iterations - 1:
-        print("i = {}\n".format(i))
-        print("\tTraining Cost: {}".format(sess.run(cost, feed_dict={inputdata: X_train, one_hot: Y_train})))
-        print("\tTraining Accuracy: {}".format(sess.run(accuracy, feed_dict={inputdata: X_train, one_hot: Y_train})))
-        print("\tValidation Cost: {}".format(sess.run(cost, feed_dict={inputdata: X_valid, one_hot: Y_valid})))
-        print("\tValidation Accuracy: {}".format(sess.run(accuracy, feed_dict={inputdata: X_valid, one_hot: Y_valid})))
+        if i % iterations == 0 or i == iterations - 1:
+            print("i = {}\n".format(i))
+            print("\tTraining Cost: {}".format(
+                sess.run(
+                    cost,
+                    feed_dict={inputdata: X_train, one_hot: Y_train})))
+            print("\tTraining Accuracy: {}".format(
+                sess.run(
+                    accuracy,
+                    feed_dict={inputdata: X_train, one_hot: Y_train})))
+            print("\tValidation Cost: {}".format(
+                sess.run(
+                    cost,
+                    feed_dict={inputdata: X_valid, one_hot: Y_valid})))
+            print("\tValidation Accuracy: {}".format(
+                sess.run(
+                    accuracy,
+                    feed_dict={inputdata: X_valid, one_hot: Y_valid})))
         sess.run(train, feed_dict={inputdata: X_train, one_hot: Y_train})
+    return saver.save(sess, save_path)
