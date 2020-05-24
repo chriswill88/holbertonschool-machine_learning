5dca03327a607f5c0763ef971fc7677e43b8d1c9
chriswill88 <williechri79@gmail.com>
2020-05-24 11:44:32 +0000

    [ADD] task 7

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 
 def evaluate(X, Y, save_path):
     """ evaluates the output of a nerual network"""
-    saver = tf.train.Saver()
+    saver = tf.train.Saver
     sp = save_path
     with tf.Session() as sess:
         sess = saver.restore(sess, sp)
