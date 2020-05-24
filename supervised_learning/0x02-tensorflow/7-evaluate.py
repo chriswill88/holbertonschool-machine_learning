0739d04dd005f409a630c118466ef053c2f68528
chriswill88 <williechri79@gmail.com>
2020-05-24 10:50:12 +0000

    [ADD] task 7

---
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""this modual contains the function for task 7"""
+import tensorflow as tf
+
+
+def evaluate(X, Y, save_path):
+    """ evaluates the output of a nerual network"""
+    rest = tf.train.Saver.restore()
+    with tf.Session() as sess:
+        sess = rest(sess, save_path)
+        prediction = sess.run(predict, feed_dict={inputdata: X_valid, one_hot: Y_valid})
+        accuracy = sess.run(accuracy, feed_dict={inputdata: X_valid, one_hot: Y_valid})
+        loss = sess.run(cost, feed_dict={inputdata: X_valid, one_hot: Y_valid})
+        print("sess -> ", sess, "\npredict -> ", prediction, "\n accuracy -> ", accuracy,"\nloss -> ", loss)
