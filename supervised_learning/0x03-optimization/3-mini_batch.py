e9779d80de3326e62ee53213f6255218a810b820
chriswill88 <williechri79@gmail.com>
2020-05-25 18:33:38 +0000

    [FIX] indent error

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -45,34 +45,34 @@ def train_mini_batch(
 
         print(x, y, accu, loss)
 
-    for i in range(epochs):
-        X_train, Y_train = shuffle_data(X_train, Y_train)
-        step = 0
-        acc = ses.run(accu, feed_dict={x: X_train, y: Y_train})
-        Vacc = ses.run(acc, feed_dict={x: X_valid, y: Y_valid})
-        cost = ses.run(loss, feed_dict={x: X_train, y: Y_train})
-        Vcost = ses.run(loss, feed_dict={x: X_valid, y: Y_valid})
-        print("After {} epochs:".format(i))
-        print("\tTraining Cost: {}".format(cost))
-        print("\tTraining Accuracy: {}".format(acc))
-        print("\tValidation Cost: {}".format(Vcost))
-        print("\tValidation Accuracy: {}".format(Vacc))
+        for i in range(epochs):
+            X_train, Y_train = shuffle_data(X_train, Y_train)
+            step = 0
+            acc = ses.run(accu, feed_dict={x: X_train, y: Y_train})
+            Vacc = ses.run(acc, feed_dict={x: X_valid, y: Y_valid})
+            cost = ses.run(loss, feed_dict={x: X_train, y: Y_train})
+            Vcost = ses.run(loss, feed_dict={x: X_valid, y: Y_valid})
+            print("After {} epochs:".format(i))
+            print("\tTraining Cost: {}".format(cost))
+            print("\tTraining Accuracy: {}".format(acc))
+            print("\tValidation Cost: {}".format(Vcost))
+            print("\tValidation Accuracy: {}".format(Vacc))
 
-        for x in range(till_epoch):  # use vectorizing
-            if step != 0 and step % 100 == 0:
-                acc = ses.run(accu, feed_dict={x: X_train, y: Y_train})
-                cost = ses.run(loss, feed_dict={x: X_train, y: Y_train})
-                print("\tStep {}:".format(step))
-                print("\t\tCost: {}".format(cost))
-                print("\t\tAccuracy: {}".format(acc))
+            for x in range(till_epoch):  # use vectorizing
+                if step != 0 and step % 100 == 0:
+                    acc = ses.run(accu, feed_dict={x: X_train, y: Y_train})
+                    cost = ses.run(loss, feed_dict={x: X_train, y: Y_train})
+                    print("\tStep {}:".format(step))
+                    print("\t\tCost: {}".format(cost))
+                    print("\t\tAccuracy: {}".format(acc))
 
-            start = step * batch_size
-            if batch_size > X_train[:, start:].shape[1]:
-                end = X_train[:, start:].shape[1]
+                start = step * batch_size
+                if batch_size > X_train[:, start:].shape[1]:
+                    end = X_train[:, start:].shape[1]
 
-            inp = X_train[:, start:end]
-            ypt = Y_train[:, start:end]
+                inp = X_train[:, start:end]
+                ypt = Y_train[:, start:end]
 
-            ses.run(train, feed_dict={x: inp, y: ypt})
-            step += 1
-    return saver.save(ses, sp)
+                ses.run(train, feed_dict={x: inp, y: ypt})
+                step += 1
+        return saver.save(ses, sp)
