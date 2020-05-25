0d3cb3fd503b6df4b22c82007c8823949ad84b2e
chriswill88 <williechri79@gmail.com>
2020-05-25 21:11:22 +0000

    [TEST]

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -61,6 +61,8 @@ def train_mini_batch(
                         end = start + batch_size
                     inp = X_train[start:end, :]
                     ypt = Y_train[start:end, :]
+                    print("start {}\nend {}\narray {}".format(start, end, inp))
+
                     if step != 0 and step % 100 == 0:
                         acc = ses.run(accu, feed_dict={x: inp, y: ypt})
                         cost = ses.run(
@@ -68,6 +70,5 @@ def train_mini_batch(
                         print("\tStep {}:".format(step))
                         print("\t\tCost: {}".format(cost))
                         print("\t\tAccuracy: {}".format(acc))
-
                     ses.run(train, feed_dict={x: inp, y: ypt})
         return saver.save(ses, sp)
