508c2119a72c481c1b95e2799f19b6431276b589
chriswill88 <williechri79@gmail.com>
2020-05-25 02:29:40 +0000

    [TEST]

--- supervised_learning/0x02-tensorflow/6-main.py
+++ supervised_learning/0x02-tensorflow/6-main.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
-import warnings
-with warnings.catch_warnings():
-    warnings.filterwarnings("ignore", category=FutureWarning)
-    import tensorflow as tf
-    import numpy as np
-train = __import__('6-train').train
 
+import numpy as np
+import tensorflow as tf
+train = __import__('6-train').train
 
 def one_hot(Y, classes):
     """convert an array to a one-hot matrix"""
@@ -19,7 +16,6 @@ if __name__ == '__main__':
     Y_train = lib['Y_train']
     X_train = X_train_3D.reshape((X_train_3D.shape[0], -1))
     Y_train_oh = one_hot(Y_train, 10)
-
     X_valid_3D = lib['X_valid']
     Y_valid = lib['Y_valid']
     X_valid = X_valid_3D.reshape((X_valid_3D.shape[0], -1))
@@ -32,5 +28,5 @@ if __name__ == '__main__':
 
     tf.set_random_seed(0)
     save_path = train(X_train, Y_train_oh, X_valid, Y_valid_oh, layer_sizes,
-    activations, alpha, iterations, save_path="./model.ckpt")
+                      activations, alpha, iterations, save_path="./model.ckpt")
     print("Model saved in path: {}".format(save_path))
