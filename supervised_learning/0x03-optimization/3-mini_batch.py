63bbe91c6c1a2720733937681697986eac18291f
chriswill88 <williechri79@gmail.com>
2020-05-25 19:08:20 +0000

    [TEST]

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -9,15 +9,15 @@ def train_mini_batch(
     batch_size=32, epochs=5,
         load_path="/tmp/model.ckpt", save_path="/tmp/model.ckpt"):
     """
-        X_train is a numpy.ndarray of shape (m, 784) containing the training data
+        X_train is a ndarray of shape (m, 784) containing training data
         m is the number of data points
         784 is the number of input features
-        Y_train is a one-hot numpy.ndarray of shape (m, 10) containing the training labels
+        Y_train is a one-hot ndarray of (m, 10) containing the training labels
         10 is the number of classes the model should classify
-        X_valid is a numpy.ndarray of shape (m, 784) containing the validation data
-        Y_valid is a one-hot numpy.ndarray of shape (m, 10) containing the validation labels
+        X_valid is a ndarray of (m, 784) containing the validation data
+        Y_valid is a one-hot ndarray of (m, 10) containing/ validation labels
         batch_size is the number of data points in a batch
-        epochs is the number of times the training should pass through the whole dataset
+        epochs is the number of times the training passes through the dataset
         load_path is the path from which to load the model
         save_path is the path to where the model should be saved after training
         Returns: the path where the model was saved
@@ -29,12 +29,9 @@ def train_mini_batch(
     bs = batch_size
 
     with tf.Session() as ses:
-        print("in the ses")
         saver = tf.train.import_meta_graph(lp + ".meta")
-        print("saver -> ", saver)
         saver.restore(sess=ses, save_path=lp)
         graph = tf.get_default_graph()
-        print("ses -> ", ses)
 
         x = tf.get_collection("x")[0]
         y = tf.get_collection("y")[0]
@@ -43,9 +40,7 @@ def train_mini_batch(
         pred = tf.get_collection("y_pred")[0]
         train = tf.get_collection("train_op")[0]
 
-        print(x, y, accu, loss)
-
-        for i in range(epochs):
+        for i in range(epochs + 1):
             X_train, Y_train = shuffle_data(X_train, Y_train)
             step = 0
             ses.run(pred, feed_dict={x: X_train, y: Y_train})
@@ -59,7 +54,7 @@ def train_mini_batch(
             print("\tValidation Cost: {}".format(Vcost))
             print("\tValidation Accuracy: {}".format(Vacc))
 
-            for step in range(till_epoch):  # use vectorizing
+            for step in range(till_epoch):
                 if step != 0 and step % 100 == 0:
                     acc = ses.run(accu, feed_dict={x: X_train, y: Y_train})
                     cost = ses.run(loss, feed_dict={x: X_train, y: Y_train})
@@ -71,7 +66,7 @@ def train_mini_batch(
                 if batch_size > X_train[start:, :].shape[0]:
                     end = X_train[start:, :].shape[0]
                 else:
-                    end = start + batch_size  # dont forget
+                    end = start + batch_size
                 inp = X_train[start:end, :]
                 ypt = Y_train[start:end, :]
 
