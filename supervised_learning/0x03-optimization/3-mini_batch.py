d04b3816e5cb6d70c6f6c7a322af7fc7961f22d2
chriswill88 <williechri79@gmail.com>
2020-05-25 21:22:14 +0000

    [TEST]

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -63,7 +63,7 @@ def train_mini_batch(
                         end = start + batch_size
                     inp = X_train[start:end, :]
                     ypt = Y_train[start:end, :]
-                    # print("start {}\nend {}\narray {}".format(start, end, inp))
+                    print("start {}\nend {}\narray {}".format(start, end, inp))
 
                     if step != 0 and step % 100 == 0:
                         acc = ses.run(accu, feed_dict={x: inp, y: ypt})
