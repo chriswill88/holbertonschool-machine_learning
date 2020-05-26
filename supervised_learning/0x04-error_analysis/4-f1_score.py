b1da21dafbf1ab95def34c02811daf8234bcc335
chriswill88 <williechri79@gmail.com>
2020-05-26 22:04:51 +0000

    [UPDATE] use precreated functions for percision and sensitivity

--- supervised_learning/0x04-error_analysis/4-f1_score.py
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -1,22 +1,12 @@
 #!/usr/bin/env python3
 """this function calculates the f1 score"""
 import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
 
 
 def f1_score(confusion):
     """calculates the f1 score for each class in the matrix"""
-    classes = len(confusion)
-    truth = 0
-    per = np.zeros((classes,))
-    rec = np.zeros((classes,))
-
-    total = np.sum(confusion)
-    for i in range(len(confusion)):
-        tp = confusion[i][i]
-        fp = np.sum(confusion[:, i]) - tp
-        fn = np.sum(confusion[i]) - tp
-        tn = total - tp - fp - fn
-
-        per[i] = tp / (tp + fp)
-        rec[i] = tp/(tp + fn)
+    per = precision(confusion)
+    rec = sensitivity(confusion)
     return 2*((per*rec)/(per+rec))
