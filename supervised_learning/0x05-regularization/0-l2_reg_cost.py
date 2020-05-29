91755dee4420486c7b4e8d14418cab27dd41dcff
chriswill88 <williechri79@gmail.com>
2020-05-29 18:49:47 +0000

    [FIX] task 0 - use np function

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -5,9 +5,9 @@ import numpy as np
 
 def l2_reg_cost(cost, lambtha, weights, L, m):
     """the cost of a neural network with L2 regularization"""
-    summ = 0
+    w = 0
     for n, value in weights.items():
-        if n[0] == "W":
-            summ += np.sum(value**2)
+        if n[0] == 'W':
+            w += np.linalg.norm(value)
 
-    return (cost) + lambtha/(2 * m) * summ**(1/2)
+    return ((cost) + (lambtha/(2 * m)) * w)
