a4af5fbb1c6916866ac9110f98bd7401c46706a1
chriswill88 <williechri79@gmail.com>
2020-05-29 22:28:21 +0000

    [TEST] alter first cost

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -9,6 +9,7 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """
     updates the weights and biases of a neural network using gradient descent with L2 regularization:
     """
+
     m = Y.shape[1]
     A = cache["A{}".format(L)]
     DZ = A - Y
@@ -21,7 +22,7 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         DW = (DZ @ PreA.T)/m + (lambtha/m*w)
         DB = np.sum(DZ, axis=1, keepdims=True)/m + (lambtha/m*w)
         da = w.T @ DZ
-        DZ = da @ (A*(1-A))
+        DZ = da * (A*(1-A))
 
-        w -= alpha * DW
-        b -= alpha * DB
+        w -= (alpha * DW)
+        b -= (alpha * DB)
