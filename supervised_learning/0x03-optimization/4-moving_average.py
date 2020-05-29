5e7ced0e2f01c5ae0e0ae4b02bf571d85fd729ec
chriswill88 <williechri79@gmail.com>
2020-05-29 02:27:35 +0000

    [ADD] taksk 4

--- supervised_learning/0x03-optimization/4-moving_average.py
+++ supervised_learning/0x03-optimization/4-moving_average.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-import numpy as np
 """this function is for task 4"""
+import numpy as np
 
 
 def moving_average(data, beta):
@@ -8,7 +8,7 @@ def moving_average(data, beta):
     moveave = np.zeros(len(data))
     prev = data[0]
     for i in range(len(data)):
-        Vt = beta * prev + (1 - beta) * data[i]
+        Vt = (beta * prev) + ((1 - beta) * data[i])
         prev = Vt
         moveave[i] = Vt
     return moveave
