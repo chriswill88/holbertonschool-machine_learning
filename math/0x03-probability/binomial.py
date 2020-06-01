fa399e94e52a295cc9709bb10a792447c9de975b
chriswill88 <williechri79@gmail.com>
2020-06-01 20:03:18 +0000

    [FIX] binomial: output

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -6,9 +6,9 @@ class Binomial:
     """Binomial is the class for binomial distribution"""
     def __init__(self, data=None, n=1, p=0.5):
         if data is None:
-            if n < 0:
+            if n <= 0:
                 raise ValueError("n must be a positive value")
-            if 0 > p > 1:
+            if 0 >= p > 1:
                 raise ValueError("p must be greater than 0 and less than 1")
             self.n = int(n)
             self.p = float(p)
