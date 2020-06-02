08ea00c64c74546f57b35691a7e1c85aef3f5e75
chriswill88 <williechri79@gmail.com>
2020-06-02 19:08:17 +0000

    [AFT] add for testing - task 0 and readme

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -1,12 +1,40 @@
 #!/usr/bin/env python3
-import tensorflow as tf
-from tensorflow import keras
+import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    model = keras.Sequential()
-    
-    for i in range(len(layers)):
-        model.add(layers.Dense(layers[i], activation=activations[i]))
+    """
+    nx is the number of input features to the network
+    layers is a list containing the number of nodes in each layer of the network
+    activations is a list containing the activation functions used for each layer of the network
+    lambtha is the L2 regularization parameter
+    keep_prob is the probability that a node will be kept for dropout
+    """
+    model = K.Sequential()
+    # creating regulizer variable - applies to every layer
+    L2 = K.regularizers.l2(lambtha)
+
+    # creating Dropout for layer
+    Dp = K.layers.Dropout(1 - keep_prob)
+
+    # adding first layer with input shape
+    ip = K.layers.Dense(
+        layers[0],
+        activation=activations[0],
+        input_shape=(None, nx),
+        kernel_regularizer=L2
+        )
+    model.add(ip)
+
+    for i in range(len(layers) - 1):
+        # dropout goes after first layer
+        # and before other layers are added
+        model.add(Dp)
+        # adding depth to neural network - adding layers
+        model.add(K.layers.Dense(
+            layers[i + 1],
+            activation=activations[i + 1],
+            kernel_regularizer=L2
+        ))
 
     return model
