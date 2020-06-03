0e5d3217403ddc2e5841fea2463a63b266796937
chriswill88 <williechri79@gmail.com>
2020-06-03 10:43:40 +0000

    [ADD] task 11

---
+++ supervised_learning/0x06-keras/11-config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""this modual contains two function save/load models"""
+import tensorflow.keras as K
+import json
+
+
+def save_config(network, filename):
+    """save_model - saves a model json"""
+    netjson = network.to_json()
+    with open(filename, "w") as files:
+        files.write(netjson)
+
+
+def load_config(filename):
+    """load_model - load a model json"""
+    with open(filename, 'r') as files:
+        modeljson = files.read()
+        return K.models_from_json(modeljson)
