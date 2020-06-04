f3c64b37379b8937010daaea74b63dd2593652e3
chriswill88 <williechri79@gmail.com>
2020-06-04 22:50:09 +0000

    [FIX] task 0 add relavant variables

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -10,13 +10,13 @@ def convolve_grayscale_valid(images, kernel):
     w = images.shape[2]
     kh = kernel.shape[0]
     kw = kernel.shape[1]
-    new = np.zeros((m, h-kh+1, w-kw+1))
+    nh, nw = h-kh+1, w-kw+1
+    new = np.zeros((m, nh, nw))
 
-    for row in range(h):
-        for col in range(w):
-            if col+kh < h and row+kw < w:
-                part = images[:, row:row+kh, col:col+kw]
-                suma = np.sum(kernel * part, axis=(1, 2))
-                new[:, row, col] = suma
+    for row in range(nh):
+        for col in range(nw):
+            part = images[:, row:row+kh, col:col+kw]
+            suma = np.sum(kernel * part, axis=(1, 2))
+            new[:, row, col] = suma
 
     return new
