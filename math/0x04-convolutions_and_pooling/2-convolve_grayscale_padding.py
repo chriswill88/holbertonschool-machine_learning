94929795fc77844faa1bbae9dd918b59fc800a30
chriswill88 <williechri79@gmail.com>
2020-06-05 09:01:19 +0000

    [ADD] task 2

--- math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -15,7 +15,7 @@ def convolve_grayscale_padding(images, kernel, padding):
 
     hi = h - 2*ph - kh + 1
     wi = w - 2*pw - kh + 1
-    new = np.zeros((m, h, w))
+    new = np.zeros((m, hi, wi))
     images = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant')
 
     for row in range(hi):
