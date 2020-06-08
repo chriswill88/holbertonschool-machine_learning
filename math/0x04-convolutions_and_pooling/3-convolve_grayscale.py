9dbba4f1c8ab7c7ad653640d0670a48236d853e2
chriswill88 <williechri79@gmail.com>
2020-06-08 00:43:22 +0000

    [ADD] task 3

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -16,42 +16,26 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     sw = stride[1]
 
     if isinstance(padding, tuple):
-        print("custom")
         ph = padding[0]
         pw = padding[1]
 
-        hi = int(h + 2 * ph - kh/sh) + 1
-        wi = int(w + 2 * pw - kw/sw) + 1
-
-        mh = hi = h + 2 * ph - kh + 1
-        mw = wi = w + 2 * pw - kw + 1
-
     elif padding is 'same':
-        ph = max(int((kh-1)/2), int(kh/2))
-        pw = max(int((kw-1)/2), int(kw/2))
-
-        hi = mh = h
-        wi = mw = w
+        ph = int((kh-1)/2)
+        pw = int((kw-1)/2)
 
     elif padding == 'valid':
         ph, pw = 0, 0
 
-        hi = int((h-kh)/sh) + 1
-        wi = int((w-kw)/sw) + 1
-
-        mh = h-kh+1
-        mw = w-kw+1
+    hi = np.floor_divide((h + 2*ph - kh), sh) + 1
+    wi = np.floor_divide((w + 2*pw - kw), sw) + 1
 
     new = np.zeros((m, hi, wi))
     newimage = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant')
 
-    r = 0
-    for row in range(0, mh, sh):
-        c = 0
-        for col in range(0, mw, sw):
-            part = newimage[:, row:row+kh, col:col+kw]
+    for row in range(hi):
+        for col in range(wi):
+            part = newimage[:, row * sh:(row*sh) + kh, col * sw:(col*sw) + kw]
             suma = np.sum(kernel * part, axis=(1, 2))
-            new[:, r, c] = suma
-            c += 1
-        r += 1
+            new[:, row, col] = suma
+
     return new
