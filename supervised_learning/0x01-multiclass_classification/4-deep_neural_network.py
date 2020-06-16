[FIX+TEST] task 4: fixing gradient descent

supervised_learning/0x01-multiclass_classification/4-deep_neural_network.py
@@ -115,7 +115,8 @@ class DeepNeuralNetwork:
 
             DW = (DZ @ PreA.T)/m
             DB = np.sum(DZ, axis=1, keepdims=True)/m
-            da = W["W{}".format(lay + 1)].T * DZ
+            if act == 'sig':
+                da = W["W{}".format(lay + 1)].T * DZ
 
             w -= alpha * DW
             b -= alpha * DB