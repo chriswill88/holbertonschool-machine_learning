[FIX+TEST] task 4: fixing gradient descent

supervised_learning/0x01-multiclass_classification/4-deep_neural_network.py
@@ -94,22 +94,23 @@ class DeepNeuralNetwork:
         A = C["A{}".format(L)]
         act = self.__activation
         # depending on the activation we use slightly diffrent code
-        da = -1 * (Y/A)+(1-Y)/(1-A)
-        if act == 'tanh':
-            DZ = Y - A
-        for lay in reversed(range(L)):
-            w = W["W{}".format(lay + 1)]
-            b = W["b{}".format(lay + 1)]
+        DZ = A - Y
+        # da = -1 * (Y/A)+(1-Y)/(1-A)
+
+        for lay in reversed(range(L + 1)):
+            w = W["W{}".format(lay)]
+            b = W["b{}".format(lay)]
             A = C["A{}".format(lay)]
             PreA = C["A{}".format(lay - 1)]
 
-            # derivative of activation
-            if act == 'sig':
-                DZ = da * (A*(1-A))
             DW = (DZ @ PreA.T)/m
             DB = np.sum(DZ, axis=1, keepdims=True)/m
-            da = W["W{}".format(lay + 1)].T @ DZ
-            if act == 'tanh':
+            da = W["W{}".format(lay)].T @ DZ
+            # derivative of activation
+
+            if act == 'sig':
+                DZ = da * (A*(1-A))
+            else:
                 DZ = da * (1 - A**2)
 
             w -= alpha * DW