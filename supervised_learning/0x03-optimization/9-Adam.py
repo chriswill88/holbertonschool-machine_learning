ee95420b1459969913a48903939ebe04d6e04046
chriswill88 <williechri79@gmail.com>
2020-06-18 05:30:51 +0000

    [ADD] task 9

--- supervised_learning/0x03-optimization/9-Adam.py
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -21,7 +21,8 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
         @t
             is the time step used for bias correction
 
-        Returns: the updated variable, the new first moment, and the new second moment, respectively@
+        Returns: the updated variable, the new first moment, and the new
+        second moment, respectively@
     """
     vdv = beta1 * v + (1 - beta1) * grad
     sdv = beta2 * s + (1 - beta2) * grad ** 2
