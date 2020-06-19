4c9b91e0009a71be78a22ab22c5d2d807a9de24b
chriswill88 <williechri79@gmail.com>
2020-06-19 03:42:12 +0000

    [TEST] task 4

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -5,9 +5,9 @@ import tensorflow as tf
 
 def calculate_accuracy(y, y_pred):
     """
-    y is a placeholder for the labels of the input data
-    y_pred is a tensor containing networks predictions
-    returns: tensor containing decimal accuracy of the prediction
+        y is a placeholder for the labels of the input data
+        y_pred is a tensor containing networks predictions
+        returns: tensor containing decimal accuracy of the prediction
     """
     mean = tf.math.reduce_mean
 
