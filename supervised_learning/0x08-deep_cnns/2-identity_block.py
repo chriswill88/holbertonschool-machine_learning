9d5093174170cd6930d5f24d1f9aabca2002b99a
chriswill88 <williechri79@gmail.com>
2020-06-21 21:27:01 +0000

    [ADD] task 2

--- supervised_learning/0x08-deep_cnns/2-identity_block.py
+++ supervised_learning/0x08-deep_cnns/2-identity_block.py
@@ -5,14 +5,18 @@ import tensorflow.keras as K
 
 def identity_block(A_prev, filters):
     """
-        builds an identity block as described in Deep Residual Learning for Image Recognition (2015):
+        builds an identity block as described in
+         Deep Residual Learning for Image Recognition (2015):
 
         A_prev is the output from the previous layer
         filters is a tuple or list containing F11, F3, F12, respectively:
         F11 is the number of filters in the first 1x1 convolution
         F3 is the number of filters in the 3x3 convolution
         F12 is the number of filters in the second 1x1 convolution
-        All convolutions inside the block should be followed by batch normalization along the channels axis and a rectified linear activation (ReLU), respectively.
+        All convolutions inside the block should be followed by batchs
+         normalization
+         along the channels axis and a rectified linear activation (ReLU),
+          respectively.
         All weights should use he normal initialization
         Returns: the activated output of the identity block
     """
@@ -37,5 +41,5 @@ def identity_block(A_prev, filters):
 
     ep = K.layers.Add()([ep, A_prev])
     ep = K.layers.Activation('relu')(ep)
-    
+
     return ep
