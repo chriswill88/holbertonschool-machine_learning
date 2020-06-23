809af9fc29aadb3cccc0e1ba4c2672035ef6cad1
chriswill88 <williechri79@gmail.com>
2020-06-23 09:42:46 +0000

    [ADD] task 1

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -7,19 +7,26 @@ import numpy as np
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """
-        updates the weights and biases of a neural network using gradient descent with L2 regularization:
+        updates the weights and biases of a neural network using
+        gradient descent with L2 regularization:
 
-        @Y is a one-hot numpy.ndarray of shape (classes, m) that contains the correct labels for the data
+        @Y is a one-hot numpy.ndarray of shape (classes, m)
+          that contains the correct labels for the data
         @@classes is the number of classes
         @m is the number of data points
-        @weights is a dictionary of the weights and biases of the neural network
-        @cache is a dictionary of the outputs of each layer of the neural network
+        @weights is a dictionary of the weights and
+         biases of the neural network
+        @cache is a dictionary of the outputs of each
+         layer of the neural network
         @alpha is the learning rate
         @lambtha is the L2 regularization parameter
         @L is the number of layers of the network
 
-        The neural network uses tanh activations on each layer except the last, which uses a softmax activation
-        The weights and biases of the network should be updated in place
+        The neural network uses tanh activations on each
+         layer except the last, which uses a softmax
+         activation
+        The weights and biases of the network should be
+         updated in place
     """
 
     m = Y.shape[1]
@@ -30,7 +37,7 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         PreA = cache["A{}".format(lay)]
 
         if lay == L - 1:
-                DZ = A - Y
+            DZ = A - Y
         else:
             A = cache["A{}".format(lay + 1)]
             DZ = da * (1 - A**2)
