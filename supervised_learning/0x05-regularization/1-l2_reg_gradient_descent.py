8684a029e6796058600880a0cb9289fe34b6337d
chriswill88 <williechri79@gmail.com>
2020-06-23 09:36:17 +0000

    [ADD] task 1

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -7,22 +7,37 @@ import numpy as np
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """
-    updates the weights and biases of a neural network using gradient descent with L2 regularization:
+        updates the weights and biases of a neural network using gradient descent with L2 regularization:
+
+        @Y is a one-hot numpy.ndarray of shape (classes, m) that contains the correct labels for the data
+        @@classes is the number of classes
+        @m is the number of data points
+        @weights is a dictionary of the weights and biases of the neural network
+        @cache is a dictionary of the outputs of each layer of the neural network
+        @alpha is the learning rate
+        @lambtha is the L2 regularization parameter
+        @L is the number of layers of the network
+
+        The neural network uses tanh activations on each layer except the last, which uses a softmax activation
+        The weights and biases of the network should be updated in place
     """
 
     m = Y.shape[1]
     A = cache["A{}".format(L)]
-    DZ = A - Y
-    for l in reversed(range(L)):
-        w = weights["W{}".format(l + 1)]
-        b = weights["b{}".format(l + 1)]
-        A = cache["A{}".format(l + 1)]
-        PreA = cache["A{}".format(l)]
+    for lay in reversed(range(L)):
+        w = weights["W{}".format(lay + 1)]
+        b = weights["b{}".format(lay + 1)]
+        PreA = cache["A{}".format(lay)]
 
-        DW = (DZ @ PreA.T)/m + (lambtha/m) * w
-        DB = np.sum(DZ, axis=1, keepdims=True)/m + (lambtha/m*w)
-        da = w.T @ DZ
-        DZ = da @ (1-A**2)
+        if lay == L - 1:
+                DZ = A - Y
+        else:
+            A = cache["A{}".format(lay + 1)]
+            DZ = da * (1 - A**2)
 
-        w -= (alpha * DW)
-        b -= (alpha * DB)
+        DW = (DZ @ PreA.T)/m
+        DB = np.sum(DZ, axis=1, keepdims=True)/m
+        da = weights["W{}".format(lay + 1)].T @ DZ
+
+        weights["W{}".format(lay + 1)] -= alpha * DW
+        weights["b{}".format(lay + 1)] -= alpha * DB
