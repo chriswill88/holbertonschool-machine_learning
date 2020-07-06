2cb253077af465dc1d5e09a3e9c146f770fc6325
chriswill88 <williechri79@gmail.com>
2020-07-06 19:10:52 +0000

    [ADD] task 1

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -45,54 +45,56 @@ class Yolo:
 
     def process_outputs(self, outputs, image_size):
         """
-        processes outputs
-        @outputs: list of np.ndarray containing the predictions from Darknet
-        model for a single image - shape below
-        (grid_height, grid_width, anchor_boxes, 4 + 1 + classes)
-            @grid_height: heigh of ouput grid
-            @grid_width: wdith of output grid
-            @4: => (t_x, t_y, t_w, t_h)
-            @1: => box_confidence
-            @classes: class probabilities for all classes
-        @image_size: np.ndarray containing the image original
-        size [image_height, image_width]
-        Returns: tuple (boxes, box_confidences, box_class_probs)
-            @boxes: np.ndarray shape (grid_height, grid_width, anchor_boxes,
-            4) the processed boundary boxes for each output
-                @4: => (x1, y1, x2, y2)
-                (x1, y1, x2, y2) represents the boundary box relative to
-                original size
-            @box_confidence: np.ndarray shape (grid_height, grid_width,
-            anchor_boxes, 1) the box confidence for each output
-            @box_class_probs: np.ndarray shape (grid_height, grid_width,
-            anchor_boxes, classes) box's class probabilities for each output
+        outputs is a list of numpy.ndarrays containing the predictions from
+         the Darknet model for a single image:
+            Each output will have the shape (grid_height, grid_width,
+             anchor_boxes, 4 + 1 + classes)
+                grid_height & grid_width => the height and width of the grid
+                 used for the output
+                anchor_boxes => the number of anchor boxes used
+                4 => (t_x, t_y, t_w, t_h)
+                1 => box_confidence
+                classes => class probabilities for all classes
+        image_size is a numpy.ndarray containing the image’s original size
+         [image_height, image_width]
+
+        Returns a tuple of (boxes, box_confidences, box_class_probs):
+            boxes: a list of numpy.ndarrays of shape (grid_height, grid_width,
+             anchor_boxes, 4) containing the processed boundary boxes for each
+             output, respectively:
+            4 => (x1, y1, x2, y2)
+            (x1, y1, x2, y2) should represent the boundary box relative to
+             original image
+            box_confidences: a list of numpy.ndarrays of shape (grid_height,
+             grid_width, anchor_boxes, 1) containing the box confidences for
+             each output, respectively
+            box_class_probs: a list of numpy.ndarrays of shape (grid_height,
+             grid_width, anchor_boxes, classes) containing the box’s class
+             probabilities for each output, respectively
         """
+        image_height, image_width = image_size
         boxes = []
         box_confidences = []
         box_class_probs = []
 
-        image_height = image_size[0]
-        image_width = image_size[1]
-
-        for i, output in enumerate(outputs):
-            gh = output.shape[0]
-            gw = output.shape[1]
-
-            t_x = output[:, :, :, 0]
-            t_y = output[:, :, :, 1]
-            t_w = output[:, :, :, 2]
-            t_h = output[:, :, :, 3]
-
-            anchor = self.anchors[i]
-
+        for n, box in enumerate(outputs):
+            anchor = self.anchors[n]
             pw = anchor[:, 0]
             ph = anchor[:, 1]
-            pw = pw.reshape(1, 1, len(pw))
-            ph = ph.reshape(1, 1, len(ph))
 
+            pw = pw.reshape((1, 1, len(pw)))
+            ph = ph.reshape((1, 1, len(ph)))
+
+            gh, gw, anchor_box, _ = box.shape
+
+            # cx, cy - the grids values - no need for for loops
             cx = np.tile(np.arange(gw), gh).reshape(gw, gw, 1)
-            cy = np.tile(np.arange(gw),
-                         gh).reshape(gh, gh, 1).T.reshape(gh, gh, 1)
+            cy = np.tile(np.arange(gh), gw).reshape(1, gw, gh).T
+
+            t_x = box[:, :, :, 0]
+            t_y = box[:, :, :, 1]
+            t_h = box[:, :, :, 2]
+            t_w = box[:, :, :, 3]
 
             bx = self.sigmoid(t_x) + cx
             by = self.sigmoid(t_y) + cy
@@ -100,28 +102,29 @@ class Yolo:
             bw = pw * np.exp(t_w)
             bh = ph * np.exp(t_h)
 
-            # normalize
-            bx = bx / gw
-            by = by / gh
+            bx = bx/gw
+            by = by/gh
+
             bw = bw / int(self.model.input.shape[1])
             bh = bh / int(self.model.input.shape[2])
 
-            x1 = (bx - bw / 2) * image_width
-            y1 = (by - bh / 2) * image_height
-            x2 = (bx + bw / 2) * image_width
-            y2 = (by + bh / 2) * image_height
+            # x1, x2, y1, y2 - corners
+            y1 = (by - bh/2) * image_height
+            x1 = (bx - bw/2) * image_width
+            x2 = (bw/2 + bx) * image_width
+            y2 = (bh/2 + by) * image_height
 
-            box = np.zeros(output[:, :, :, :4].shape)
-            box[:, :, :, 0] = x1
-            box[:, :, :, 1] = y1
-            box[:, :, :, 2] = x2
-            box[:, :, :, 3] = y2
+            b_size = np.zeros((gh, gw, anchor_box, 4))
+            b_size[:, :, :, 0] = x1
+            b_size[:, :, :, 1] = y1
+            b_size[:, :, :, 2] = x2
+            b_size[:, :, :, 3] = y2
+            boxes.append(b_size)
 
-            boxes.append(box)
+            # box confidences
+            box_confidences.append(self.sigmoid(box[:, :, :, 4:5]))
 
-            box_confidence = self.sigmoid(output[:, :, :, 4, np.newaxis])
-            box_confidences.append(box_confidence)
+            # box class
+            box_class_probs.append(self.sigmoid(box[:, :, :, 5:]))
 
-            box_class = self.sigmoid(output[:, :, :, 5:])
-            box_class_probs.append(box_class)
-        return boxes, box_confidences, box_class_probs
+        return (boxes, box_confidences, box_class_probs)
