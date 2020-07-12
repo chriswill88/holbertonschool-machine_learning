5a3c69650aeeb92ad9811cfe23d0847cf3697a07
chriswill88 <williechri79@gmail.com>
2020-07-11 23:11:01 -0400

    [ADD] task 100

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -4,25 +4,11 @@
 
 def np_slice(matrix, axes={}):
     """slices np arrays and returns them"""
-    start, stop, end = None, None, None
     slc = [slice(None, None, None)] * len(matrix.shape)
     mat = matrix.copy()
-    ax = len(matrix)
 
     for a, slic in axes.items():
-        try:
-            start = slic[0] if 0 < ax else None
-        except IndexError:
-            pass
-        try:
-            stop = slic[1] if 1 < ax else None
-        except IndexError:
-            pass
-        try:
-            end = slic[2] if 2 < ax else None
-        except IndexError:
-            pass
-        slc[a] = slice(start, stop, end)
+        slc[a] = slice(*slic)
 
     mat = mat[tuple(slc)]
     return mat
