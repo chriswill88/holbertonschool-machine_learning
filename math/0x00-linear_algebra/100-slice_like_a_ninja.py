d51ec593f6a65ef42871380ed834e606be0b2858
chriswill88 <williechri79@gmail.com>
2020-07-11 21:45:52 -0400

    [ADD] task 100

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""this modual contains the function for task 100"""
+import numpy as np
+
+
+def np_slice(matrix, axes={}):
+    """slices np arrays and returns them"""
+    start, stop, end = None, None, None
+
+    ax = len(matrix)
+    for a, slic in axes.items():
+        try:
+            start = slic[0] if 0 < ax else None
+        except IndexError:
+            pass
+
+        try:
+            stop = slic[1] if 1 < ax else None
+        except IndexError:
+            pass
+        try:
+            end = slic[2] if 2 < ax else None
+        except IndexError:
+            pass
+
+        slc = [slice(None)] * len(matrix.shape)
+        slc[a] = slice(start, stop, end)
+        matrix = matrix[slc]
+    return matrix
