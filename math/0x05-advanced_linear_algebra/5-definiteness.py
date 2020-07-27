d9bc62ae0baa379370de8d6910d6a47dd1947a12
chriswill88 <williechri79@gmail.com>
2020-07-26 22:50:09 -0400

    [ADD] task 5

--- math/0x05-advanced_linear_algebra/5-definiteness.py
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -1,23 +1,6 @@
 #!/usr/bin/env python3
 """This module contains the function for how to solve a determinant"""
-
-
-def check(matrix):
-    """
-    The error checker
-    returns size of square
-    """
-    size = len(matrix)
-
-    if not isinstance(matrix, list) or size == 0:
-        raise TypeError("matrix must be a list of lists")
-
-    for row in matrix:
-        if not isinstance(row, list):
-            raise TypeError("matrix must be a list of lists")
-        if len(row) != size and size != 1 or not len(row):
-            raise ValueError("matrix must be a non-empty square matrix")
-    return size
+import numpy as np
 
 
 def submatrix(matrix, index):
@@ -27,13 +10,6 @@ def submatrix(matrix, index):
 
     return [[matrix[x][i] for i in range(
            size) if i != index] for x in range(size - 1)]
-
-
-def minOfMatrix(matrix, h, w):
-    """This function creates a list of list of minors from indices"""
-    size = len(matrix)
-    return [[matrix[x][i] for i in range(
-            size) if i != h] for x in range(size) if x != w]
 
 
 def det(matrix, size):
@@ -50,56 +26,6 @@ def det(matrix, size):
             sum += mul * (matrix[0][i] * det(submatrix(matrix, i), size - 1))
             mul *= -1
     return sum
-
-
-def inverse(matrix):
-    """
-    This function calculates the adjugate
-    @matrix is a list of lists whose determinant should be calculated
-
-    If matrix is not a list of lists, raise a TypeError with the message
-     matrix must be a list of lists
-    If matrix is not square or is empty, raise a ValueError
-     with the message matrix must be a non-empty square matrix
-    The list [[]] represents a 0x0 matrix
-
-    Returns: the adjugate of matrix
-    """
-    # check function
-    size = check(matrix)
-    d = det(matrix, size)
-    if d == 0:
-        return None
-    sub = []
-
-    # matrix of minors
-    if size == 1:
-        return [[1/d]]
-    if size == 2:
-        sub = [sub[::-1] for sub in matrix[::-1]]
-        sub[0][1] *= -1
-        sub[1][0] *= -1
-        sub[0][1], sub[1][0] = sub[1][0], sub[0][1]
-        return [[x/d for x in i] for i in sub]
-
-    else:
-        sub = [[det(minOfMatrix(matrix, x, i), size - 1)
-                for i in range(size)] for x in range(size)]
-    # cofactors
-    mul = 1
-    for i in range(len(sub)):
-        for x in range(len(sub)):
-            sub[i][x] *= mul
-            mul *= -1
-
-    # adjugate
-    size = len(sub)
-    adj = [[sub[x][i] for x in range(size)] for i in range(size)]
-
-    # inverse
-    d = det(matrix, size)
-    size = len(adj)
-    return [[adj[x][i]/d for x in range(size)] for i in range(size)]
 
 
 def definiteness(matrix):
@@ -119,7 +45,6 @@ def definiteness(matrix):
 
     You may import numpy as np
     """
-    import numpy as np
     lis = [
         "Positive definite", "Positive semi-definite",
         "Negative semi-definite", "Negative definite", "Indefinite"]
@@ -146,6 +71,3 @@ def definiteness(matrix):
         else:
             pick = -1
     return lis[pick]
-
-
-
