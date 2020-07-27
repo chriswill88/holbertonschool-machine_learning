ed9113d17e0b0b78f262e5156280894731271132
chriswill88 <williechri79@gmail.com>
2020-07-27 01:55:30 -0400

    [ADD] task 5

--- math/0x05-advanced_linear_algebra/5-definiteness.py
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -20,38 +20,4 @@ def definiteness(matrix):
 
     You may import numpy as np
     """
-    lis = [
-        "Positive definite", "Positive semi-definite",
-        "Negative semi-definite", "Negative definite", "Indefinite"]
-
-    if not isinstance(matrix, np.ndarray):
-        raise TypeError("matrix must be a numpy.ndarray")
-
-    size = matrix.shape[0]
-    if len(matrix.shape) == 1 or matrix.shape[0] != matrix.shape[1]:
-        return None
-
-    pick = 0
-    w, v = np.linalg.eig(matrix)
-    for i in w:
-        if i < 0:
-            pick = 2
-    if pick == 0:
-        return lis[0]
-
-    for i in range(size):
-        mat = matrix[:i + 1, :i + 1]
-        d = det(mat, i + 1)
-        if d > 0 and pick == 0:
-            pick = 0
-        elif (i % 2 == 0 and d < 0) or (i % 2 == 1 and d > 0 and pick != 0):
-            pick = 3
-        elif d == 0 and (pick == 0 or pick == 1):
-            pick = 1
-        elif d == 0 and (pick == 3 or pick == 2):
-            pick = 2
-        else:
-            pick = -1
-    return lis[pick]
-
-
+    return 0
