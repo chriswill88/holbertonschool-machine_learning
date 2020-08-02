b1948dcbab065e564537e4bc3f9b4bbce25faaa4
chriswill88 <williechri79@gmail.com>
2020-08-02 18:53:46 -0400

    [ADD] task 0 fix conditionals

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -20,7 +20,7 @@ def likelihood(x, n, P):
         raise ValueError("x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError("x cannot be greater than n")
-    if not isinstance(P, np.ndarray) and len(P.shape) != 1:
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
     if len(np.where(P < 0)[0]) or len(np.where(P > 1)[0]):
         raise ValueError("All values in P must be in the range [0, 1]")
