7e140d3071565e57326963d201a81f754d433d20
chriswill88 <williechri79@gmail.com>
2020-08-06 04:36:29 -0400

    [ADD] task 0

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -7,9 +7,12 @@ def initialize(X, k):
     """
     initialize: initializes cluster centroids for K-mean
     """
-    n, d = X.shape
+    try:
+        n, d = X.shape
 
-    mini = X.min(axis=0)
-    maxi = X.max(axis=0)
+        mini = X.min(axis=0)
+        maxi = X.max(axis=0)
 
-    return np.random.uniform(mini, maxi, (k, d))
+        return np.random.uniform(mini, maxi, (k, d))
+    except Exception:
+        return None
