8136875bbf6e88af8b4fd0bc2a7c7d8ff332581f
chriswill88 <williechri79@gmail.com>
2020-08-06 16:30:54 -0400

    [ADD] task 1

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -15,24 +15,22 @@ def kmeans(X, k, iterations=1000):
     maxi = X.max(axis=0)
 
     init = np.random.uniform(mini, maxi, (k, d))
-    return recurKmean(iterations, init, X)
-
-
-def recurKmean(iterations, init, X):
-    """uses recursion to calculate kmean"""
     kLabels = np.zeros(X.shape[0],)
-    centroid = init.copy()
-    dist = np.sqrt(((X - centroid[:, np.newaxis])**2).sum(axis=2))
-    kLabels = np.argmin(dist, 0)
-    for c3, ele3 in enumerate(init):
-        cor = X[np.where(kLabels == c3)]
-        if len(cor) > 0:
-            new = np.mean(cor, 0)
-        else:
-            new = np.random.uniform(
-                X.min(axis=0), X.max(axis=0), (1, X.shape[1]))
-        centroid[c3] = new
-    compare = centroid == init
-    if not compare.all() and iterations > 1:
-        return recurKmean(iterations - 1, centroid, X)
+    while iterations:
+        centroid = init.copy()
+        dist = np.sqrt(((X - centroid[:, np.newaxis])**2).sum(axis=2))
+        kLabels = np.argmin(dist, 0)
+        for c3, ele3 in enumerate(init):
+            cor = X[np.where(kLabels == c3)]
+            if len(cor) > 0:
+                new = np.mean(cor, 0)
+            else:
+                new = np.random.uniform(
+                    X.min(axis=0), X.max(axis=0), (1, X.shape[1]))
+            centroid[c3] = new
+        compare = centroid == init
+        if compare.all():
+            break
+        init = centroid
+        iterations -= 1
     return centroid, kLabels
