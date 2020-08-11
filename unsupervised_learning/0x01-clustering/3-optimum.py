0c5e328f87d62e10ab8a20f26bd28118906fbcfa
chriswill88 <williechri79@gmail.com>
2020-08-11 16:47:58 -0400

    [ADD]

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -21,10 +21,14 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     try:
         results = []
         d_vars = []
-        if kmin < 1 or kmin > kmax or kmax < 1:
-            return None, None
+
         if kmax is None:
             kmax = X.shape[0]
+
+        if not isinstance(kmin, int) or kmin < 1:
+            return None, None
+        if not isinstance(kmax, int) or kmax < 1 or kmin > kmax:
+            return None, None
 
         for k in range(kmin, kmax + 1):
             result = kmeans(X, k, iterations)
