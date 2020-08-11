d45fe9eaa5e0c98bfde879fd47c7f38fe34fe1df
chriswill88 <williechri79@gmail.com>
2020-08-11 01:58:27 -0400

    [ADD] task 2

---
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""this modual contains the function for task 2"""
+import numpy as np
+
+
+def variance(X, C):
+    """calculates the total for a data set"""
+    n, d = X.shape
+    k = C.shape[0]
+    hold = np.zeros((k, n))
+    lis = np.zeros((n))
+
+    for j in range(k):
+        for i in range(n):
+            var = ((X[i] - C[j])**2).sum(0)
+            if lis[i] == 0 or var < lis[i]:
+                lis[i] = var
+
+    lis = np.amin(lis.sum())
+
+    return lis
