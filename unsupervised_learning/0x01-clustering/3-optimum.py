045412c00f120a401d2dde38e1ebad45c55f0a57
chriswill88 <williechri79@gmail.com>
2020-08-11 16:22:58 -0400

    [ADD]

---
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""This modual contains the function for task 3"""
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    """
+        That tests for the optimum number of clusters by variance:
+
+        X is a numpy.ndarray of shape (n, d) containing the data set
+        kmin is a positive integer containing the minimum number of clusters
+         to check for (inclusive)
+        kmax is a positive integer containing the maximum number of clusters
+         to check for (inclusive)
+        iterations is a positive integer containing the maximum number of
+         iterations for K-means
+        This function should analyze at least 2 different cluster sizes
+    """
+    results = []
+    d_vars = []
+    for k in range(kmin, kmax + 1):
+        result = kmeans(X, k, iterations)
+        results.append(result)
+        d_vars.append(variance(X, result[0]))
+    d_vars = [d_vars[0] - i for i in d_vars]
+    return results, d_vars
