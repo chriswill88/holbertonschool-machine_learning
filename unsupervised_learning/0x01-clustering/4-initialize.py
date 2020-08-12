2e241929e679f29d49644a45fccd86da71dc065c
chriswill88 <williechri79@gmail.com>
2020-08-11 23:00:27 -0400

    [ADD] task 4

--- unsupervised_learning/0x01-clustering/4-initialize.py
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -9,7 +9,7 @@ def initialize(X, k):
 
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
-    if not isinstance(k, int) or k < 0:
+    if not isinstance(k, int) or k < 1:
         return None, None, None
 
     m, _ = kmeans(X, k)
