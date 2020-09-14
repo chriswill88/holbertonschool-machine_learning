ecd56e275ba5840d5a783bd23aefcf8fdaecd01f
chriswill88 <williechri79@gmail.com>
2020-09-14 04:20:14 -0400

    [ADD] task 5

--- unsupervised_learning/0x01-clustering/5-pdf.py
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -5,6 +5,19 @@ import numpy as np
 
 def pdf(X, m, S):
     """Calc the pdf of a gaussian distribution"""
+
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+    if not isinstance(m, np.ndarray) or len(X.shape) != 1:
+        return None
+    if not isinstance(S, np.ndarray) or len(X.shape) != 2:
+        return None
+
+    if X.shape[1] != m.shape[0] or X.shape[1] != S.shape[0]:
+        return None
+    if S.shape[0] != S.shape[1]:
+        return None
+
     n = m.shape[0]
     Sigma_det = np.linalg.det(S)
     Sigma_inv = np.linalg.inv(S)
