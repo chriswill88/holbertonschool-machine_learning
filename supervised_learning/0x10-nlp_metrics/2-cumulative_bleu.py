1672c96e26f3e85fa67e309026e4d1dbff28368c
chriswill88 <williechri79@gmail.com>
2020-09-24 15:12:29 -0400

    [ADD]

--- supervised_learning/0x10-nlp_metrics/2-cumulative_bleu.py
+++ supervised_learning/0x10-nlp_metrics/2-cumulative_bleu.py
@@ -18,29 +18,36 @@ def ngram_bleu(references, sentence, n):
     lref = min([len(i) for i in references])
 
     parsed_sent = nparser(sentence, n)
+    # print(parsed_sent)
 
     parsed_tot = len(parsed_sent)
 
     parsed_ref = []
     for i in references:
         parsed_ref.append(nparser(i, n))
+    # print(parsed_ref)
 
     uniq_ref = {}
     for i in parsed_ref:
         for x in i:
             if x not in uniq_ref:
                 uniq_ref[x] = max([l.count(x) for l in parsed_ref])
+    # print("ur", uniq_ref)
 
     uniq_words = {}
     for phrase in parsed_sent:
+        # print("phrase ->", phrase)
         if phrase not in uniq_words and phrase in uniq_ref:
             if parsed_sent.count(phrase) >= uniq_ref[phrase]:
                 uniq_words[phrase] = uniq_ref[phrase]
             else:
                 uniq_words[phrase] += 1
     find = sum(uniq_words.values())
+    # print(find)
+    # print(uniq_words)
     bp = 1 if total >= lref else np.exp(1 - (lref/total))
-
+    # print("bp->", bp)
+    # print(find, total)
     return find/parsed_tot
 
 
@@ -52,9 +59,8 @@ def cumulative_bleu(references, sentence, n):
     total = len(sentence)
     for i in range(n):
         score_hold.append(ngram_bleu(references, sentence, i + 1))
-
-    m = np.log(score_hold)
-    m = np.exp(np.average(m))
-
-    bp = 1 if total <= lref else np.exp(1 - (lref/total))
-    return m * bp
+    # print(score_hold)
+    # print(np.average(score_hold))
+    bp = 1 if total >= lref else np.exp(1 - (lref/total))
+    # print("bp ->", bp)
+    return np.exp(np.average(np.log(score_hold))) * bp
