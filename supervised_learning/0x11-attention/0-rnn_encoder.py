c6f9f9dd65004a120fde99b637dfcecfdaec2fe0
chriswill88 <williechri79@gmail.com>
2020-09-25 06:40:16 -0400

    [ADD ALL]

--- supervised_learning/0x11-attention/0-rnn_encoder.py
+++ supervised_learning/0x11-attention/0-rnn_encoder.py
@@ -6,6 +6,8 @@ import tensorflow as tf
 class RNNEncoder(tf.keras.layers.Layer):
     """RNNEncoder - class for rnn"""
     def __init__(self, vocab, embedding, units, batch):
+        super(RNNEncoder, self).__init__()
+
         self.batch = batch
         self.units = units
 
