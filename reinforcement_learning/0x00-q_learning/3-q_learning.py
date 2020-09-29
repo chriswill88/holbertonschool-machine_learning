6ddda5c65ad0fa2960c42c1b65a5403f6792147a
chriswill88 <williechri79@gmail.com>
2020-09-29 17:46:06 -0400

    [ADD]

--- reinforcement_learning/0x00-q_learning/3-q_learning.py
+++ reinforcement_learning/0x00-q_learning/3-q_learning.py
@@ -18,26 +18,24 @@ def train(
     env, Q, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99, epsilon=1,
         min_epsilon=0.1, epsilon_decay=0.05):
     """This function trains Q-learning algorithm"""
-    epi = epsilon
     total_rewards = []
+    maxeps = epsilon
     for i_episode in range(episodes):
+        curr_rew = 0
         state = env.reset()
-
         for t in range(max_steps):
-            # env.render()
             action = epsilon_greedy(Q, state, epsilon)
             new_state, reward, done, info = env.step(action)
             if reward == 0 and done is True:
                 reward = -1
             Q[state, action] = Q[state, action] + alpha * (
                 reward + gamma * np.max(Q[new_state, :]) - Q[state, action])
+
             state = new_state
+            curr_rew += reward
             if done:
-                total_rewards.append(reward)
-                # print("Episode finished after {} timesteps".format(t+1))k
-                if (epsilon - epsilon_decay) > min_epsilon:
-                    epsilon -= epsilon_decay
-                else:
-                    epsilon = min_epsilon
+                total_rewards.append(curr_rew)
                 break
+        epsilon = min_epsilon + \
+            (maxeps - min_epsilon) * np.exp(-epsilon_decay*i_episode)
     return Q, total_rewards
