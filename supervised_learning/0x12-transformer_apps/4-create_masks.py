ffc23680a15a6c2691ce06d4b4781a4f849fc183
chriswill88 <williechri79@gmail.com>
2020-10-11 13:16:22 -0400

    [ADD task 4]

--- supervised_learning/0x12-transformer_apps/4-create_masks.py
+++ supervised_learning/0x12-transformer_apps/4-create_masks.py
@@ -7,14 +7,20 @@ def create_masks(inputs, target):
     """
     That creates all masks for training/validation:
 
-    @inputs: is a tf.Tensor of shape (batch_size, seq_len_in) that contains the input sentence
-    @target: is a tf.Tensor of shape (batch_size, seq_len_out) that contains the target sentence
+    @inputs: is a tf.Tensor of shape (batch_size, seq_len_in) that contains
+     the input sentence
+    @target: is a tf.Tensor of shape (batch_size, seq_len_out) that contains
+     the target sentence
 
-    This function should only use tensorflow operations in order to properly function in the training step
+    This function should only use tensorflow operations in order to properly
+     function in the training step
     Returns: encoder_mask, look_ahead_mask, decoder_mask
-        encoder_mask is the tf.Tensor padding mask of shape (batch_size, 1, 1, seq_len_in) to be applied in the encoder
-        look_ahead_mask is the tf.Tensor look ahead mask of shape (batch_size, 1, seq_len_out, seq_len_out) to be applied in the decoder
-        decoder_mask is the tf.Tensor padding mask of shape (batch_size, 1, 1, seq_len_in) to be applied in the decoder
+        encoder_mask is the tf.Tensor padding mask of shape
+         (batch_size, 1, 1, seq_len_in) to be applied in the encoder
+        look_ahead_mask is the tf.Tensor look ahead mask of shape
+         (batch_size, 1, seq_len_out, seq_len_out) to be applied in the decoder
+        decoder_mask is the tf.Tensor padding mask of shape
+         (batch_size, 1, 1, seq_len_in) to be applied in the decoder
     """
     batch_size, seq_len_in = inputs.get_shape()
     _, seq_len_out = target.get_shape()
