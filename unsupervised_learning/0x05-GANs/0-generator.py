46251cb11eb5e5192e68570a3604e32ccefcea67
chriswill88 <williechri79@gmail.com>
2020-10-14 17:42:49 -0400

    [ADD] task 0 and README

---
+++ unsupervised_learning/0x05-GANs/0-generator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""this module contains the function for task 0"""
+import tensorflow as tf
+import numpy as np
+
+
+def generator(Z):
+    """this function creates a generator and returns a tensor"""
+    with tf.variable_scope("generator", reuse=tf.AUTO_REUSE):
+        layer_1 = tf.keras.layers.Dense(128, 'relu',)(Z)
+        layer_2 = tf.keras.layers.Dense(784, 'sigmoid')(layer_1)
+        layer_1.name("Layer_1")
+        layer_2.name("Layer_2")
+        model = tf.keras.Model(inputs=Z, outputs=layer_2)
+        x = model(Z)
+    return x
