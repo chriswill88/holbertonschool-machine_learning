e1e5139d9799779c68dea45532fa102355c4330c
chriswill88 <williechri79@gmail.com>
2020-10-15 20:29:46 -0400

    [ADD] task 7

--- supervised_learning/0x11-attention/7-transformer_encoder_block.py
+++ supervised_learning/0x11-attention/7-transformer_encoder_block.py
@@ -13,12 +13,6 @@ class EncoderBlock(tf.keras.layers.Layer):
         self.mha = MultiHeadAttention(dm, h)
         self.dense_hidden = tf.keras.layers.Dense(hidden, 'relu')
         self.dense_output = tf.keras.layers.Dense(dm)
-        self.ffn = tf.keras.Sequential([
-            tf.keras.layers.Dense(hidden, activation='relu'),
-            # (batch_size, seq_len, dff)
-            tf.keras.layers.Dense(dm)
-            # (batch_size, seq_len, d_model)
-        ])
 
         self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
         self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
@@ -36,7 +30,7 @@ class EncoderBlock(tf.keras.layers.Layer):
         out1 = self.layernorm1(x + attn_output)
         # (batch_size, input_seq_len, dm)
 
-        ffn_output = self.ffn(out1)  # (batch_size, input_seq_len, dm)
+        ffn_output = self.dense_output(self.dense_hidden(out1))  # (batch_size, target_seq_len, dm)
         ffn_output = self.dropout2(ffn_output, training=training)
         out2 = self.layernorm2(out1 + ffn_output)
         # (batch_size, input_seq_len, dm)
