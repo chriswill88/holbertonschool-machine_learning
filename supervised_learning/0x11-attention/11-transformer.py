6464f67be1a92d34c5305a25ce3f6f54b2dcd68e
chriswill88 <williechri79@gmail.com>
2020-10-15 22:22:34 -0400

    [ADD]

--- supervised_learning/0x11-attention/11-transformer.py
+++ supervised_learning/0x11-attention/11-transformer.py
@@ -8,9 +8,14 @@ Decoder = __import__('10-transformer_decoder').Decoder
 class Transformer(tf.keras.Model):
     """this class is a representation of a Transformer"""
     def __init__(
-        self, N, dm, h, hidden, input_vocab, 
+        self, N, dm, h, hidden, input_vocab,
             target_vocab, max_seq_input, max_seq_target, drop_rate=0.1):
-        """Transformer class"""
+        """
+            Transformer class:
+            @encoder - the encoder layer
+            @decoder - the decoder layer
+            @linear - a final Dense layer with target_vocab units
+        """
         super(Transformer, self).__init__()
 
         self.encoder = Encoder(
@@ -24,7 +29,10 @@ class Transformer(tf.keras.Model):
     def call(
         self, inputs, target, training, encoder_mask,
             look_ahead_mask, decoder_mask):
-        """this function runs the transformer"""
+        """
+        call:
+            this function runs the transformer
+        """
         enc_output = self.encoder(inputs, training, encoder_mask)
         # (batch_size, inp_seq_len, dm)
         # dec_output.shape == (batch_size, tar_seq_len, dm)
