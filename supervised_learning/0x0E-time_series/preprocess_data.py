757ed9a98e6562c09dc88ea3f5c450ae746b6ee4
chriswill88 <williechri79@gmail.com>
2020-10-16 09:09:53 -0400

    [FIX] added documentation

--- supervised_learning/0x0E-time_series/preprocess_data.py
+++ supervised_learning/0x0E-time_series/preprocess_data.py
@@ -5,7 +5,11 @@ import tensorflow as tf
 
 
 class PreprocessData:
+    """This class contains tools that are used to preprocess the data"""
     def __init__(self, data):
+        """
+        @data is the raw dataset
+        """
         self.raw_data = data
 
         self.train_data, self.test_data, self.scalar\
@@ -17,6 +21,14 @@ class PreprocessData:
         self.tf_test = tf.data.Dataset.from_tensor_slices(self.test_data)
 
     def preprocess_data(self, bit):
+        """
+        preprovess_data - this function preprocesses the dataset:
+
+        1. it truncats the dataset to just a single column
+        2. it also limits the data to show a point every hour
+        3. it scales the data between 0 and 1
+        4. it parses the data into a training set and test set
+        """
         # hourly data, removeing nulls, only close column
         bit = bit[::60]
         bit = bit.where(pd.notna(bit), bit.mean(), axis='columns')
@@ -38,6 +50,7 @@ class PreprocessData:
         return (trainX, trainY), (testX, testY), scalar
 
     def dataset(self, timestep, data):
+        """this function parse the data by certain timesteps"""
         dataX, dataY = [], []
         for i in range(len(data) - timestep):
             a = data[i:i+timestep]
@@ -46,5 +59,6 @@ class PreprocessData:
         return np.array(dataX), np.array(dataY)
 
     def unprocess_data(self, data):
+        """this unprocesses the data"""
         scalar = self.scalar
         return scalar.inverse_transform(data)
