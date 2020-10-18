cefdcf61dec46285f361b401bb5c7ee670dbbb92
chriswill88 <williechri79@gmail.com>
2020-10-18 18:25:14 -0400

    [ADD]

--- supervised_learning/0x0E-time_series/forecast_btc.py
+++ supervised_learning/0x0E-time_series/forecast_btc.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 """this module will be used to forcast_btc"""
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import keras as K
-from datetime import datetime
-from sklearn.preprocessing import MinMaxScaler
+# import matplotlib.pyplot as plt
+from tensorflow.keras.optimizers import Adam
+import tensorflow as tf
 pp = __import__('preprocess_data').PreprocessData
 
 """
@@ -18,35 +16,84 @@ a 60 second time window containing:
 """
 
 # open and view csv file
-bit = pd.read_csv('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv')
+bit = np.genfromtxt(
+    'coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', delimiter=',')
+
+# bit = pd.read_csv('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv')
+# bit = pd.read_csv('bitstampUSD_1-min_data_2012-01-01_to_2020-04-22.csv')
 data = pp(bit)
 
 trainX, trainY = data.train_data
+# print(trainX.shape)
+validX, validY = data.valid_data
 testX, testY = data.test_data
 
-# train_tf = data.tf_train
-# test_tf = data.tf_test
+train_tf = data.tf_train
+valid_tf = data.tf_valid
+test_tf = data.tf_test
 
-
+print("tx shape", trainX.shape)
 # Model
-model = K.Sequential()
-model.add(K.layers.LSTM(1, return_sequences=True, input_shape=(24, 1)))
-model.add(K.layers.LSTM(1, return_sequences=True))
-model.add(K.layers.LSTM(1))
-model.add(K.layers.Dense(1))
-model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.LSTM(1, input_shape=(24, 1)))
+model.add(tf.keras.layers.Dense(1))
+model.compile(loss='mse', optimizer=Adam(lr=.00007), metrics=['accuracy'])
 model.summary()
 
-model.fit(trainX, trainY, epochs=20, batch_size=64, validation_split=.2)
+# print("before train", train_tf)
+history = model.fit(
+    trainX, trainY, validation_data=(
+        validX, validY), batch_size=64, epochs=20).history
 model.save('forecast.h5')
+# print("Saved model")
+# print(history)
 
-# prediction
-train_pred = model.predict(trainX)
-test_pred = model.predict(testX)
+# history
+acc = history['acc']
+val_acc = history['val_acc']
+loss = history['loss']
+val_loss = history['val_loss']
 
-train_pred = data.unprocess_data(train_pred)
-test_pred = data.unprocess_data(test_pred)
-testY = data.unprocess_data(testY)
+# Prediction
 
-for i in range(len(testY)):
-    print(testY[i], "vs", test_pred[i])
+# train_pred = model.predict(trainX)
+# test_pred = model.predict(testX)
+# train_pred = []
+# for i in range(len(trainX)):
+#     train_pred.append(model.predict(trainX[np.newaxis, i]))
+
+# test_pred = []
+# for i in range(len(testX)):
+#     test_pred.append(model.predict(testX[np.newaxis, i]))
+# \
+train_pred = np.squeeze(np.array(train_pred), 1)
+# print(train_pred.shape)
+test_pred = np.squeeze(np.array(test_pred), 1)
+# print(test_pred.shape)
+# train_pred = data.unprocess_data(train_pred)
+# test_pred = data.unprocess_data(test_pred)
+# testY = data.unprocess_data(testY)
+
+# plt.figure(1)
+# plt.title("train loss")
+# plt.plot(loss, label="train")
+# plt.plot(val_loss, label="val")
+
+# plt.figure(3)
+# plt.title("train")
+# plt.plot(trainY, label="trainY")
+# plt.plot(test_pred,  label="train_pred")
+
+# plt.figure(4)
+# plt.title("test")
+# plt.plot(test_pred, label='test_pred', )
+# plt.plot(train_pred[:, 0], label="test")
+
+# plt.figure(5)
+# plt.title("pred comp")
+# plt.plot(data.scaled_sdata, label="data")
+# # plt.plot(train_pred, label="test")
+# # plt.plot(test_pred,  label="train")
+# plt.legend()
+
+# plt.show()
--- supervised_learning/0x0E-time_series/preprocess_data.py
+++ supervised_learning/0x0E-time_series/preprocess_data.py
@@ -1,6 +1,4 @@
 import numpy as np
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 
 
@@ -11,43 +9,65 @@ class PreprocessData:
         @data is the raw dataset
         """
         self.raw_data = data
-
-        self.train_data, self.test_data, self.scalar\
-            = self.preprocess_data(data)
-
-        self.tf_train = tf.data.Dataset.from_tensor_slices(
-            self.train_data)
-
-        self.tf_test = tf.data.Dataset.from_tensor_slices(self.test_data)
-
-    def preprocess_data(self, bit):
-        """
-        preprovess_data - this function preprocesses the dataset:
-
-        1. it truncats the dataset to just a single column
-        2. it also limits the data to show a point every hour
-        3. it scales the data between 0 and 1
-        4. it parses the data into a training set and test set
-        """
+        # print(data.shape)
         # hourly data, removeing nulls, only close column
-        bit = bit[::60]
-        bit = bit.where(pd.notna(bit), bit.mean(), axis='columns')
-        bit = bit['Close']
+        self.cdata = data[:, 1]  # close column
+        # print(self.cdata.shape)
+        self.tdata = self.cdata[::60]  # hour interval data
+        # print(self.tdata.shape)
 
-        # scaling data 0, 1
-        scalar = MinMaxScaler(feature_range=(0, 1))
-        scaledBit = scalar.fit_transform(np.array(bit).reshape(-1, 1))
+        self.sdata = self.moving_average(self.tdata, 20)[:, np.newaxis]
+        # smooth over using moving ave
+        # print("sdata", self.sdata.shape)
+        # mean and std for normalization
+        self.mean = None
+        self.std = None
 
-        # Train/validation/test ordered by time
-        train_size = int(len(scaledBit)*.7)
+        # train/valid/test
+        self.train_data, self.valid_data, self.test_data \
+            = self.preprocess_data(self.sdata)
 
+        # scaled sdata
+        self.scaled_sdata = self.scale_data(self.sdata)
+
+        # tensor dataset
+        self.tf_train = tf.data.Dataset.from_tensor_slices(
+            self.train_data).batch(32)
+        self.tf_valid = tf.data.Dataset.from_tensor_slices(
+            self.valid_data).batch(32)
+        self.tf_test = tf.data.Dataset.from_tensor_slices(
+            self.test_data).batch(32)
+
+    def preprocess_data(self, sdata):
+        """
+        preprocess_data - this function preprocesses the dataset:
+        """
+        n = len(sdata)
         # parsing datasets
-        train_data, test_data = scaledBit[:train_size], scaledBit[train_size:]
+        train_data, valid_data, test_data = sdata[:int(n*.7)], \
+            sdata[int(n*.7):int(n*.9)], sdata[int(n*.9):]
+
+        # setting mean and std for normalization
+        self.mean = np.mean(train_data)
+        self.std = np.std(train_data)
+
+        # normalize data
+        train_data = self.scale_data(train_data)
+        valid_data = self.scale_data(valid_data)
+        test_data = self.scale_data(test_data)
 
         # 24 hour data set: 1 hour after y values
         trainX, trainY = self.dataset(24, train_data)
+        validX, validY = self.dataset(24, valid_data)
         testX, testY = self.dataset(24, test_data)
-        return (trainX, trainY), (testX, testY), scalar
+
+        # print(trainX.shape, trainY.shape)
+        return (trainX, trainY), (validX, validY), (testX, testY)
+
+    def moving_average(self, a, n=3):
+        ret = np.nancumsum(a, dtype=float)
+        ret[n:] = ret[n:] - ret[:-n]
+        return ret[n - 1:] / n
 
     def dataset(self, timestep, data):
         """this function parse the data by certain timesteps"""
@@ -58,7 +78,9 @@ class PreprocessData:
             dataY.append(data[i + timestep])
         return np.array(dataX), np.array(dataY)
 
-    def unprocess_data(self, data):
+    def scale_data(self, data):
+        return (data - self.mean)/self.std
+
+    def unscale_data(self, data):
         """this unprocesses the data"""
-        scalar = self.scalar
-        return scalar.inverse_transform(data)
+        return (data + self.mean)*self.std
